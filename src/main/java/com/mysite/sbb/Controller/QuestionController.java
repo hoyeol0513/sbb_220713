085c31df6e989a1d6456f7d5f2ddccd709273cfd
hoyeol0513 <hoyeol0513@naver.com>
2022-07-14 10:25:25 +0900

    question thymeleaf html templete

--- src/main/java/com/mysite/sbb/Controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/Controller/QuestionController.java
@@ -1,13 +1,9 @@
 package com.mysite.sbb.Controller;
 
 import com.mysite.sbb.Repository.QuestionRepository;
-import com.mysite.sbb.vo.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/question")
@@ -16,8 +12,8 @@ public class QuestionController {
     private QuestionRepository questionRepository;
 
     @RequestMapping("/list")
-    @ResponseBody
-    public List<Question> showQuestions(){
-        return questionRepository.findAll();
+    //@ResponseBody를 떼면 알아서 템플릿을 찾아감.
+    public String showQuestions(){
+        return "question_list";
     }
 }
