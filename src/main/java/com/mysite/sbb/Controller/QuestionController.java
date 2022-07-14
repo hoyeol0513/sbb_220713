074048733094ad62a2badc87af6987f66447096b
hoyeol0513 <hoyeol0513@naver.com>
2022-07-14 10:48:06 +0900

    thymeleaf model & template html setting

--- src/main/java/com/mysite/sbb/Controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/Controller/QuestionController.java
@@ -1,19 +1,26 @@
 package com.mysite.sbb.Controller;
 
 import com.mysite.sbb.Repository.QuestionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mysite.sbb.vo.Question;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/question")
+//autowired 없이도 자동으로 생성자를 만들어줌.
+@AllArgsConstructor
 public class QuestionController {
-    @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/list")
     //@ResponseBody를 떼면 알아서 템플릿을 찾아감.
-    public String showQuestions(){
+    public String showQuestions(Model model){
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
