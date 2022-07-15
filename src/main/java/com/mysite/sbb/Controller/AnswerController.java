3c3da8319af28cd6340f25c369a468551076cc66
hoyeol0513 <hoyeol0513@naver.com>
2022-07-15 11:07:00 +0900

    createAnswer & create AnswerService

--- src/main/java/com/mysite/sbb/Controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/Controller/AnswerController.java
@@ -1,23 +1,30 @@
 package com.mysite.sbb.Controller;
 
-import com.mysite.sbb.Repository.AnswerRepository;
-import com.mysite.sbb.vo.Answer;
+import com.mysite.sbb.service.AnswerService;
+import com.mysite.sbb.service.QuestionService;
+import com.mysite.sbb.vo.Question;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/answer")
+@AllArgsConstructor //final이 된 answerService를 받기위함.
 public class AnswerController {
     @Autowired
-    private AnswerRepository answerRepository;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
 
-    @RequestMapping("list")
-    @ResponseBody
-    public List<Answer> showAnswer(){
-        return answerRepository.findAll();
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content){
+        Question question = this.questionService.getQuestion(id); //해당 질문에 대한 답변이 필요
+        //질문만들기
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
     }
 }
---
+++ src/main/java/com/mysite/sbb/service/AnswerService.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.service;
+
+import com.mysite.sbb.Repository.AnswerRepository;
+import com.mysite.sbb.vo.Answer;
+import com.mysite.sbb.vo.Question;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
