eb2b11f3d8d6d53240d36080d9e1cd1d5bcf8b6c
hoyeol0513 <hoyeol0513@naver.com>
2022-07-17 12:07:25 +0900

    refactor domain

---
+++ src/main/java/com/mysite/sbb/Article/ArticleRepository.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb.Article;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findByTitle(String title);
+    boolean existsByTitle(String title);
+    List<Article> findByBody(String body);
+    boolean existsByBody(String body);
+    List<Article> findByTitleAndBody(String title, String body);
+    boolean existsByTitleAndBody(String title, String body);
+}
