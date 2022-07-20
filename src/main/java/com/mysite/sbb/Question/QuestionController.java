135d2583b68dfb3a9802f7e862424bff4cd9b591
hoyeol0513 <hoyeol0513@naver.com>
2022-07-20 11:05:21 +0900

    add qustion_form & createQuestion in Controller

--- src/main/java/com/mysite/sbb/Question/QuestionController.java
+++ src/main/java/com/mysite/sbb/Question/QuestionController.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,9 +24,20 @@ public class QuestionController {
     }
 
     @RequestMapping("/detail/{id}")
-    public String showDetail(Model model, @PathVariable("id") Integer id){
+    public String showDetail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
-        model.addAttribute("question",question);
+        model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String qustionCreate(){
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content){
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/mysite/sbb/Question/QuestionService.java
+++ src/main/java/com/mysite/sbb/Question/QuestionService.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.Util.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,13 @@ public class QuestionService {
         }else{ //예외처리
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content){
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question);
     }
 }
