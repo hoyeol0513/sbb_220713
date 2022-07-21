3bf599dfa4b78f4f284decff4576b2acdda3a5a6
hoyeol0513 <hoyeol0513@naver.com>
2022-07-21 13:05:55 +0900

    paging desc(createDate)

--- src/main/java/com/mysite/sbb/Question/QuestionController.java
+++ src/main/java/com/mysite/sbb/Question/QuestionController.java
@@ -3,16 +3,13 @@ package com.mysite.sbb.Question;
 import com.mysite.sbb.Answer.AnswerForm;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/question")
@@ -23,9 +20,10 @@ public class QuestionController {
     private QuestionService questionService;
     @RequestMapping("/list")
     //@ResponseBody를 떼면 알아서 템플릿을 찾아감.
-    public String showQuestions(Model model){
-        List<Question> questionList = questionService.getList();
-        model.addAttribute("questionList",questionList);
+    public String showQuestions(Model model, @RequestParam(value="page", defaultValue="0") int page){
+        //페이지 별로 리스트를 가져오도록 수정하기 paging으로
+        Page<Question> paging = questionService.getList(page);
+        model.addAttribute("paging",paging);
         return "question_list";
     }
 
